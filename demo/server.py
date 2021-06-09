e22c659f1f9031dc59efb8e27c881bf98b7d7d56
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-06-09 21:40:33 +0700

    Initial commit

---
+++ demo/server.py
@@ -0,0 +1,33 @@
+import socket
+
+HOST = socket.gethostbyname(socket.gethostname())  # Standard loopback interface address (localhost)
+PORT = 65431        # Port to listen on (non-privileged ports are > 1023)
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+print(f'Created a server with IP: {HOST}, send it to your client')
+print("Waiting for connection")
+s.listen(1)
+# count = 0
+while True:
+    conn, addr = s.accept()
+    # count += 1
+    try:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            str_data = data.decode("utf8")
+            if str_data == "quit" or str_data == 'exit':
+                break
+            
+            print("Client: " + str_data)
+
+            # Server send input
+            msg = input("Server: ")
+            conn.sendall(bytes(msg, "utf8"))
+    finally:
+        # Clean up the connection
+        conn.close()
+        # if count == 2: 
+        #     break
+        s.close()
---
+++ demo/test.py
@@ -0,0 +1,8 @@
+print("helo ")
+
+shepherd = "Martha"
+age = 34
+# Note f before first quote of string
+stuff_in_string = "Shepherd %s is %d years old." % (shepherd, age)
+print(stuff_in_string)
+
