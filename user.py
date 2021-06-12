8bb2ae1ff49129bfb49e0c0a2456e0a86686163f
chanhbeck <chanhbeck27@gmail.com>
2021-06-12 14:57:30 +0700

    clean code

--- user.py
+++ user.py
@@ -1,30 +1,36 @@
-def checkUsername(newUser):
-    user_str = newUser['username']
-    for i in arr['user']:
+def checkExistUsername(user):
+    user_str = user['username']
+    for i in arr['users']:
         if user_str == i['username']:
-            return False
-    arr['user'].append(newUser)
-    return True
+            return True # đã tồn tại
+    return False # k tồn tại
+
+def checkUserPassword(user):
+    for i in arr['users']:
+        if user['username'] == i['username'] and user['password'] == i['password']:
+            return True # đúng password
+    else: return False # sai password
 
 def checkLogin(user):
-    username = user['username']
-    password = user['password']
-    for i in arr['users']:
-        if username == i['username']:
-            if password == i['password']:
-                err = 0
-                break
-            else: 
-                err = 1
-                break
-        else: err = 2
-    if err == 1: print('Your password is incorrect!')
-    elif err == 2: print('Username does not found!')
+    err = 0
+    if not checkExistUsername(user): err = 1   # k tồn tại username
+    elif not checkUserPassword(user): err = 2  # k đúng password
+    if err == 1: print('Username does not found!')
+    elif err == 2: print('Your password is incorrect!')
     else: print('Login successfully!')
 
+def createNewUser(user):
+    if not checkExistUsername(user):
+        arr['users'].append(user)
+        newF = open('data.json',"w")
+        newF.write(json.dumps(arr['users']))
+        newF.close()
+        return True # tạo user thành công
+    else: return False # tạo user thất bại
+
 def checkInputType(inp):
-   if inp.strip().isdigit(): return False
-   else: return True
+   if inp.strip().isdigit(): return False  # inp là số -> False
+   else: return True  # inp là str -> True
 
 def searchDefault(searchType, inpStr):
     if checkInputType(inpStr):
@@ -33,8 +39,8 @@ def searchDefault(searchType, inpStr):
             if i[searchType].startswith(inpStr):
                 returnArr.append(i)
         if returnArr == []: return False
-        else: return returnArr
-    else: return False
+        else: return returnArr  # return Arr nếu tồn tại kquả
+    else: return False  # return False nếu k tìm thấy
 
 def searchByName(name):
     return searchDefault('name', name)
@@ -52,11 +58,13 @@ import json
 
 f = open('data.json', "r")
 arr = json.loads(f.read())
-newArr = []
 
 user = {}
-user['username'] = '123'
-user['password'] = '123'
+user['username'] = 'admin'
+user['password'] = 'admin'
+
+checkLogin(user)
+print('\n')
 
 book = {}
 book['id'] = 1
@@ -71,3 +79,5 @@ for i in x:
     print('Type: ' + i['type'])
     print('Author: ' + i['author'])
     print('\n')
+
+f.close()
