a5cfe3d08f483856fbf5caff383f0abb7970a036
chanhbeck <chanhbeck27@gmail.com>
2021-06-16 10:23:29 +0700

    update code (zt)

--- func.py
+++ func.py
@@ -27,29 +27,27 @@ def createNewUser(user):
         return True # tạo user thành công
     else: return False # tạo user thất bại (đã tồn tại username trong json)
 
-def checkInputType(inp):
-   if inp.strip().isdigit(): return False  # inp là số -> False
-   else: return True  # inp là str -> True
-
 def searchDefault(searchType, inpStr):
-    if checkInputType(inpStr):
-        returnArr = []
-        for i in arr['books']:
-            if i[searchType].startswith(inpStr):
-                returnArr.append(i)
-        if returnArr == []: return False # return False nếu k tìm thấy
-        else: return returnArr  # return Arr nếu tồn tại kquả
-    else: return False # return False nếu inpStr k phải str
+    returnArr = []
+    for i in arr['books']:
+        if searchType == 'id':
+            i['id'] = str(i['id'])
+        if i[searchType].startswith(inpStr):
+            i['id'] = int(i['id'])
+            returnArr.append(i)
+    if returnArr == []: return False # return False nếu k tìm thấy
+    else: return returnArr  # return Arr nếu tồn tại kquả
 
 def searchBook(inpStr):
-    arr = inpStr.split(" ", 1)
+    arr = inpStr.split(' ', 1)
     for i in range(len(arr)):
         arr[i] = arr[i].strip().replace('"', '')
-    if arr[0] == "": return False   #sai cú pháp search (khoảng trắng ở đầu )
-    elif arr[0] == "F_ID": return searchByID(arr[1])
-    elif arr[0] == "F_Name": return searchByName(arr[1])
-    elif arr[0] == "F_Type": return searchByType(arr[1])
-    elif arr[0] == "F_Author": return searchByAuthor(arr[1])
+    if arr[0] == "F_ID":        arr[1] = arr[1].replace(' ', '')
+    if arr[0] == "":            return False   #sai cú pháp search (khoảng trắng ở đầu )
+    elif arr[0] == "F_ID":      return searchByID(arr[1])
+    elif arr[0] == "F_Name":    return searchByName(arr[1])
+    elif arr[0] == "F_Type":    return searchByType(arr[1])
+    elif arr[0] == "F_Author":  return searchByAuthor(arr[1])
 
 def searchByID(ID):
     return searchDefault('id', ID)
@@ -78,3 +76,5 @@ arr = json.loads(f.read())
 
 
 ########## test down this line ##########
+
+print(searchBook("F_ID 23"))
