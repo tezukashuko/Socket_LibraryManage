905922b77ef35995a7a885c7a226765ba743c566
Phu Truong <59415928+tezukashuko@users.noreply.github.com>
2021-06-27 16:18:32 +0700

    st

--- testfunction/demo/test.py
+++
@@ -1,6 +0,0 @@
-print("helo ")
-
-shepherd = 'F_Name   "Martha asdqf qwwf  qwdqwfqf"'
-# Note f before first quote of string
-newStr = shepherd.split(" ", 1)
-newStr[1] = newStr[1].strip().replace('"', '')
